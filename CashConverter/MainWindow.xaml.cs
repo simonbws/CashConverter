496c18418d10e43bb4a88b0657eac0ec38ed65c2
szymon <DESKTOP-C0TU3RB>
2022-09-12 13:40:14 +0200

    Binded items, merged and configured buttons to convert

--- CashConverter/MainWindow.xaml.cs
+++ CashConverter/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,12 +24,53 @@ namespace CashConverter
         public MainWindow()
         {
             InitializeComponent();
-            labelCurr.Content = "Click Here";
+            BindCurr();
+        }
+        private void BindCurr()
+        {
+            DataTable dtCurr = new DataTable();
+            dtCurr.Columns.Add("Text");
+            dtCurr.Columns.Add("Value");
+            
+
+            //Value and Text needs to be added to database
+            dtCurr.Rows.Add("SELECT", 0);
+            dtCurr.Rows.Add("INR", 1);
+            dtCurr.Rows.Add("USD", 75);
+            dtCurr.Rows.Add("EUR", 85);
+            dtCurr.Rows.Add("SAR", 20);
+            dtCurr.Rows.Add("POUND", 5);
+            dtCurr.Rows.Add("ZL", 0.75);
+
+            //We assign data currency to items source of CmbFromCurrency
+            cmbFromCurr.ItemsSource = dtCurr.DefaultView;
+            cmbFromCurr.DisplayMemberPath = "Text";
+            cmbFromCurr.SelectedValuePath = "Value";
+            cmbFromCurr.SelectedIndex = 0;
+
+            cmbToCurr.ItemsSource = dtCurr.DefaultView;
+            cmbToCurr.DisplayMemberPath= "Text";
+            cmbToCurr.SelectedValuePath="Value";
+            cmbToCurr.SelectedIndex = 0;
         }
 
         private void Convert_Click(object sender, RoutedEventArgs e)
         {
-            labelCurr.Content = "Button clicked";
+            double TransformedValue;
+
+            if (textCurr.Text == null || textCurr.Text.Length == 0)
+            {
+                MessageBox.Show("Please provide currency you want to transform", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                textCurr.Focus();
+                return;
+            }
+            else if (cmbFromCurr.SelectedValue == null || cmbFromCurr.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency From", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbFromCurr.Focus();
+                return;
+            }
+
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
@@ -36,7 +78,7 @@ namespace CashConverter
             labelCurr.Content = "Button clicked";
 
         }
-
+    
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             labelCurr.Content = "Button clicked";
